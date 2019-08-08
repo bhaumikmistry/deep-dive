6f30e3d0ac1e56e655972d31464e6e7d0595effe
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-07 23:55:11 -0400

    complete ll c++ code

--- DS/LinkedList/LinkedList.cpp
+++ DS/LinkedList/LinkedList.cpp
@@ -1,42 +1,91 @@
-
+#include <stddef.h>
+#include <assert.h>
 
 template<class T>
 class node
 {
     public:
-    node(const T& item,node<T>  * next=nullptr)
+    node(const T& item,node<T> * n)
     {
-        _data = T;
-        _next = next;
+        _data = item;
+        _next = n;
     }
 
     T _data;
+    node<T> * _next;
+};
 
-    void InsertAfter(T data_to_add)
-    {
-        _next = new node(data_to_add,_next);
-    }
-
-    node<T> * DeleteAfter()
-    {
-        node<T> * temp = _next;
-        if(_next!=NULL)
-        {
-            _next = _next->_next;
-        }
-        return _next;
-    }
-
+template<class T>
+class hNode
+{
     private:
-    node * _next = nullptr;    
+    int _size;
+    node<T> * _head;
+
+    void push_front(T item)
+    {
+        _size++;
+        _head = new node<T>(item,_head); 
+    }
+
+    void push_back(T item)
+    {
+        node<T> * temp = _head;
+        while (temp->_next!=NULL)
+        {
+            temp = temp->_next;                
+        }
+        temp->_next = new node<T>(item,NULL);
+        _size++;
+    }
+
+    bool empty()
+    {
+        return !(_size>0);
+    }
+
+    T value_at(int index)
+    {
+        int count =0;
+        node<T> * temp = _head;
+        while(temp!=NULL)
+        {
+            if(count==index)
+            {
+                return temp->_data;
+            }
+            count++;
+        }
+        assert(0);
+    }
+
+    void reverse()
+    {
+        node<T> * prev = NULL;
+        node<T> * next = NULL;
+        node<T> * current = _head;
+        while(current!=NULL)
+        {
+            next = current->_next;
+            current->_next = prev;
+            prev = current;
+            current = next;
+        }
+        _head = prev;
+    }
+
+    //more
+    // fornt(); //same as value_at(0);
+    // back(); // same as value_at(size-0);
+    // insert(int data,int index);
+    // erase(int index);
+    // value_from_end_(); // use help of size and get index
+    // remove_value();
 
 };
 
 
 int main( int arg, char ** args)
 {
-
-
-
     return 0;
 }

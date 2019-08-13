f2c1d42e3d1227a965e8dc92d511bbf1a103ac73
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2019-08-12 22:46:42 -0400

    add queue

--- DS/LinkedList/buildRun.sh
+++ DS/LinkedList/buildRun.sh
@@ -1,4 +1,5 @@
 clear
-g++ -c LinkedList.cpp -o LinkedList.o
-g++ -o main LinkedList.o
+g++ -c LinkedList.cpp -o ll.o
+g++ -c main.cpp -o main.o
+g++ -o main main.o ll.o
 ./main
---
+++ DS/Queue/buildrun.sh
@@ -0,0 +1,2 @@
+g++ -c Queue.cpp -o q.o
+g++ -o main q.o

04bd5b5d88024759b7b919a626a53b9ea40ff09d
bhaumikmistry <bhaumikpmistry@gmail.com>
2019-09-24 21:18:53 -0400

    empty structre for hashtable

--- DS/HashTables/Hashtable.cpp
+++ DS/HashTables/Hashtable.cpp
@@ -1,10 +1,40 @@
+#include <vector>
+#include <string>
+
+struct HashEntry{
+    int _key;
+    int _data;
+    HashEntry * _next_node;
+    HashEntry(int k,int d):_key(k),_data(d){}
+};
+
+class HashTable{
+
+    std::vector<HashEntry*> _table;
+    int _size=0;
+    HashTable(int s):_size(s){
+        _table.resize(_size);
+        for(int i =0;i<_size;i++){
+            _table[i]=nullptr;
+        }
+    }
+
+    void insertKey(int k,int d){
+
+    }
+
+    HashEntry * deleteKey(int k)
+    {
+        return nullptr;
+    }
+
+    void printKey()
+    {
+
+    }
 
 
-struct Data{
-    int key;
-    std::string data;
-}
-
+};
 
 
 int main( int args, char ** argv)
